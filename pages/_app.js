d6a35efc040c48c26a7451dc26926f539a2aaf90
ZukoHeiwa <zukoheiwagithub@gmail.com>
2023-09-19 15:04:17 +0300

    bun-nextjs-ecommerce-demo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'; // Use a relative path to import the CSS file
+import Header from './components/Header'; // Assuming that 'Header.js' is in the 'components' directory
+import Footer from './components/Footer'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
